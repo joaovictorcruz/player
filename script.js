151c718cf2d7d9fb6d4dae378640089f2ef4e812
João Victor <elietejvc@hotmail.com>
2024-08-31 02:20:36 -0300

    Manipulando JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-songName = document.getElementById('song-name');
+const songName = document.getElementById('song-name');
+const song = document.getElementById('audio');
+const play = document.getElementById('play');
+
+songName.innerText = 'Nights';
+
+function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.play();
+};
+
+play.addEventListener('click', playSong);
